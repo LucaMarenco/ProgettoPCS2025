b3ef37891dfa6e03c1ee0f052475fe6ab3a2da8b
Massimo223 <s308028@studenti.polito.it>
2025-06-06 09:55:30 +0200

    altri test

--- src_test/test_poliedro_geodetico.hpp
+++ src_test/test_poliedro_geodetico.hpp
@@ -1,13 +1,12 @@
 #include "test_poliedro_geodetico.hpp"
 #include <gtest/gtest.h>
 
-TEST(Triangolazione_I_Test, NumeroPunti) {
+TEST(Triangolazione_II_Test, NumeroPunti) {
 	Vector3d A( 0.57735027,  0.57735027,  0.57735027);
 	Vector3d B(-0.57735027, -0.57735027,  0.57735027);
 	Vector3d C(-0.57735027,  0.57735027, -0.57735027);
 	int b = 2;
-	EXPECT_EQ(punti_triangolazione(A,B,C,b).size(), 6);
-	EXPECT_EQ(punti_triangolazione_II(A,B,C,b).size(), 16);
+	EXPECT_EQ(punti_triangolazione_II_n_n(A,B,C,b).size(), 16);
 }  
 
 TEST(Triangolazione_I_II_Test, Normalizzazione) {
@@ -36,5 +35,38 @@ TEST(Id_vertici_test, Valore_id) {
 	int id_vertice = 0;
 	ofstream s_g_Cell0Ds("s_g_Cell0Ds.txt");
 	if
+	
+}
+
+TEST(Calcola_intersezione_test, Intersezione) {
+	Vector3d A(0, 0, 0);
+	Vector3d B(2, 2, 0);
+	Vector3d C(0, 2, 0);
+	Vector3d D(2, 0, 0);
+	Vector3d expected(1, 1, 0);
+	EXPECT_NEAR((Calcola_intersezione(A, B, C, D) - expected).norm(), 0.0, 1e-6);
+}
+
+TEST(Punti_lungo_i_lati_e_lungo_i_lati_n_n_test, NumeroPunti_e_Normalizzazione) {
+	int b = 2;
+	Vector3d A(0.57735027,  0.57735027,  0.57735027);
+	Vector3d B(-0.57735027, -0.57735027,  0.57735027);    
+	Vector3d C(-0.57735027,  0.57735027, -0.57735027);
+	auto points = punti_lungo_i_lati(A,B,C,b);
+	auto points_II = punti_lungo_i_lati_n_n(A,B,C,b);
+	EXPECT_EQ(points.size(), 12);
+	EXPECT_NEAR(points_II.size(), 12);
+	for(int i = 0; i < points_II.size(); i++) {
+		EXPECT_NEAR(points_II[i].norm(), 1.0, 1e-6);
+	}
+}
+
+TEST(Trova_punti_vicini, Numero_punti_e_vicinanza) {
+	Vector3d punto(-0.85065081, 0.00000000, 0.52573111);
+	vector<Vector3d> points = {{ 0.57735027,  0.57735027,  0.57735027}, {-0.57735027, -0.57735027,  0.57735027},
+	{-0.57735027,  0.57735027, -0.57735027}, { 0.57735027, -0.57735027, -0.57735027}, {0.52573111, 0.85065081, 0.00000000},
+	{-0.52573111, 0.85065081, 0.00000000}, {0.52573111, -0.85065081, 0.00000000}, {-0.52573111, -0.85065081, 0.00000000},
+	{0.85065081, 0.00000000, 0.52573111}, {-0.85065081, 0.00000000, 0.52573111}};
+	EXPECT_EQ(trova_punti_vicini(points).size(), 6);
 	
 }
